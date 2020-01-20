873e03fb1b1fae477620c6e5e8fea2f6dc21a5dd
Benjamin Floyd <benjamin.floyd@cyberark.com>
2020-01-20 14:50:03 -0600

    Adding list replicas util, some test output, and the backup master script.

--- utils/list_replicas.sh
+++ utils/list_replicas.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
-set -x
+# set -x
 
-CONJUR_CLI_IMAGE="cyberark/conjur-cli:5"
+CONJUR_CLI_IMAGE="conjurinc/cli5"
 MASTER_LB=localhost
+
+if [ "$#" == "1" ];then
+    MASTER_LB=$1
+fi
 
 # 
 docker run --name cli-util --rm --entrypoint /bin/bash $CONJUR_CLI_IMAGE -c \
-    "curl -sk https://$MASTER_LB/info | jq -c '.configuration.conjur.hostname'"
+    "curl -sk https://$MASTER_LB/health | \
+    jq -rc '.database.replication_status | 
+    if .pg_stat_replication | length != 0 then 
+        .pg_stat_replication[] | { IP: .client_addr, Type: .application_name, Sync: .sync_state } 
+    else 
+        \"No replication clients found.\" 
+    end'"
