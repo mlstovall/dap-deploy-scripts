c5e68332968c66acb223c11940a7ca6e82494ac6
Benjamin Floyd <benjamin.floyd@cyberark.com>
2020-01-24 16:09:52 -0600

    Updating backup scripts and adding crontab reference

--- utils/backup-master.sh
+++ utils/backup-master.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-if [ "$(curl -sk https://localhost/info | jq -r .configuration.conjur.role)" == "master" ];then
-    docker exec dap evoke backup
+RETENTION_DAYS=30
+
+if [ "$(curl -sk https://localhost/info | jq -r .role)" == "master" ];then
+  # delete backups older than 30 days
+  find /opt/conjur/backup -maxdepth 1 -type f -mtime +$RETENTION_DAYS -print | grep Z.tar.xz.gpg | xargs /bin/rm -f
+  # run the backup
+  docker exec dap evoke backup
 fi
