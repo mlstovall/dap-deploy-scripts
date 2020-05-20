a332a491762ae28c7951cc1bc868ac233eaa8f92
Benjamin Floyd <benjamin.floyd@cyberark.com>
2020-05-20 17:09:11 -0500

    Adding validation to 001 script

--- 001-master-configure_master.sh
+++ 001-master-configure_master.sh
@@ -19,12 +19,50 @@ prompt_confirm() {
   done
 }
 
+function validate_complexity() {
+    local MASTER_PASS=$1
+
+    CNT=$(echo "$MASTER_PASS" | wc -c)
+    CUPPER=$(echo "$MASTER_PASS" | tr -dc A-Z | wc -c)
+    CLOWER=$(echo "$MASTER_PASS" | tr -dc a-z | wc -c)
+    CDIGIT=$(echo "$MASTER_PASS" | tr -dc 0-9 | wc -c)
+    CSPECIAL=$(echo "$MASTER_PASS" | tr -dc "!\"#$%&\'()*+,-.\/:;<=>?@\[\\\]^_\`{|}~" | wc -c)
+
+    local error=0
+
+    if [ "$CNT" -lt 12 ] || [ "$CNT" -gt 128 ]; then error=1;fi
+    if [ "$CUPPER" -lt 2 ]; then error=1;fi
+    if [ "$CLOWER" -lt 2 ]; then error=1;fi
+    if [ "$CDIGIT" -lt 1 ]; then error=1;fi
+    if [ "$CSPECIAL" -lt 1 ];then error=1;fi
+
+    if [ "$error" -eq 1 ];then
+        echo "Password did not meet complexity requirements:"
+        echo " - Between 12-128 characters long"
+        echo " - 2 upper case letters"
+        echo " - 2 lower case letters"
+        echo " - 1 digit"
+        echo " - 1 special character in !\"#$%&\'()*+,-.\/:;<=>?@\[\\\]^_\`{|}~"
+
+        return 1
+    fi
+
+    return 0
+}
+
 capture_password() {
   while true; do
     read -s -p "Admin Password: " MASTER_PASS
     echo ""
+
+    if ! validate_complexity "$MASTER_PASS"; then
+      prompt_confirm "Try again?" || exit 1
+      continue
+    fi
+
     read -s -p "Confirm Pass: " MASTER_PASS1
     echo ""
+
     if [ "$MASTER_PASS" != "$MASTER_PASS1" ]; then
       prompt_confirm "Pass and confirm pass do not match. Try again?" || exit 1
       continue
